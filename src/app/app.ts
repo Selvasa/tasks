d63077e5d36e3e670b2f72c3cdabdaddcc12b15d
ponmana selvan <selvasa2277@gmail.com>
2025-11-25 12:33:43 +0530

    feature/day-1: recreate the text editor to html editor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TemplateEditor } from './components/day1-template-editor/template-editor';
+import { HTMLEditor } from './components/day1-html-editor/html-editor';
 
 @Component({
   selector: 'app-root',
-  imports: [TemplateEditor],
+  imports: [TemplateEditor,HTMLEditor],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
